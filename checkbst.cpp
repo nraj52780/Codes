8d1b1dd152bf03513b99b6671bf5fb0f3d9e6271
nraj52780 <45739384+nraj52780@users.noreply.github.com>
2019-06-12 16:59:56 +0530

    Update checkbst.cpp

--- checkbst.cpp
+++ checkbst.cpp
@@ -32,3 +32,52 @@ bool isBST(BinaryTreeNode<int> *root){
 
 }
 -------------------------------------------------------------------------------------------------
+2nd approach
+O(n)
+
+#include<bits/stdc++.h>
+class triplet
+{
+  public:
+    bool res;
+    int minm;
+    int maxm;
+};
+
+triplet fun(BinaryTreeNode<int> *root)
+{
+     if(root==NULL) 
+    {
+        triplet obj;
+        obj.res=true;
+        obj.minm=INT_MAX;
+        obj.maxm=INT_MIN;
+        
+        return obj;
+    }
+    
+    triplet leftans=fun(root->left);
+    triplet rightans=fun(root->right);
+    
+    int mx=max(root->data,max(leftans.maxm,rightans.maxm));
+    int mn=min(root->data,min(leftans.minm,rightans.minm));
+    bool res=root->data>=leftans.maxm&&root->data<=rightans.minm&&leftans.res&&rightans.res;
+    triplet obj;
+        obj.res=res;
+        obj.minm=mn;
+        obj.maxm=mx;
+        
+        return obj;
+}
+bool isBST(BinaryTreeNode<int> *root){
+       /* Don't write main().
+	* Don't read input, it is passed as function argument.
+	* Return output and don't print it.
+	* Taking input and printing output is handled automatically.
+	*/
+   triplet ans=fun(root);
+    return ans.res;
+  
+        
+
+}
